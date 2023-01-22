415c2a78a9c584fe4b952bd1849010ad151a5139
Dmitry <shevtsov.dima.03@gmail.com>
2023-01-22 21:04:34 +0300

    Add to project second poly

---
+++ VulkanV2/shaders2/compile.bat
@@ -0,0 +1,3 @@
+C:/4course/Practice4/VulkanRenderSX/VulkanV2/VulkanV2/shaders2/glslc.exe C:\4course\Practice4\VulkanRenderSX\VulkanV2\VulkanV2\shaders2\shader.vert -o vert.spv
+C:/4course/Practice4/VulkanRenderSX/VulkanV2/VulkanV2/shaders2/glslc.exe C:\4course\Practice4\VulkanRenderSX\VulkanV2\VulkanV2\shaders2\shader.frag -o frag.spv
+exit
